69651db69e534b878de1ec752b5dec61aa37ea33
Ed Denham <ed.denham@gmail.com>
2023-01-05 21:10:59 +0000

    Update ORM

--- sql-orm.py
+++ sql-orm.py
@@ -9,4 +9,37 @@ db.create_engine("postgresql:///chinook")
 base = declarative_base()
 
 
+# create a class-based Model for the "Artist" table
+class Artist(base):
+    __tablename__ = "Artist"
+    ArtistId = Column(Integer, primary_key=True)
+    Name = Column(String)
+
+
+class Album(base):
+    __tablename__ = "Album"
+    AlbumId = Column(Integer, primary_key=True)
+    Title = Column(String)
+    ArtistId = Column(Integer, ForeignKey("Artist.ArtistId"))
+
+
+class Track(base):
+    __tablename__ = "Track"
+    TrackId = Column(Integer, primary_key=True),
+    Name = Column(String),
+    AlbumId = Column(Integer, ForeignKey("Album.AlbumId"))
+    MediaTypeId = Column(Integer, primary_key=False)
+    GenreId = Column(Integer, primary_key=False)
+    Composer = Column(String)
+    Milliseconds = Column(Integer)
+    Bytes = Column(Integer)
+    Price = Column(Float)
+
+
+
+# Creates instance of a session
 Session = sessionmaker(db)
+# Opens a session
+session = Session()
+# Creates teh databse using declarative_base subclass
+base.metadata.create_all(db)
